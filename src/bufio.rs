8b7d3bc9755d5a6c4008935c8eba675a224140d6
Trbe <780553323@qq.com>
2024-03-29 23:02:27 +0800

    update

--- src/bufio.rs
+++ src/bufio.rs
@@ -1,19 +1,84 @@
-use std::io::{BufReader, BufWriter, Read, Write};
+use std::io::{self, BufReader, BufWriter, Read, Seek, SeekFrom, Write};
 
 #[derive(Debug)]
-pub(super) struct BufReaderWithPos<R>
-where
-    R: Read,
-{
+pub(super) struct BufReaderWithPos<R: Read> {
     pos: u64,
     reader: BufReader<R>,
 }
 
+impl<R: Read + Seek> BufReaderWithPos<R> {
+    pub(super) fn new(mut r: R) -> io::Result<Self> {
+        let pos = r.stream_position()?;
+        let reader = BufReader::new(r);
+        Ok(Self { pos, reader })
+    }
+}
+
+impl<R: Read> BufReaderWithPos<R> {
+    fn read(&self) -> u64 {
+        self.pos
+    }
+}
+
+impl<R: Read> Read for BufReaderWithPos<R> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.reader.read(buf).map(|bytes_read| {
+            self.pos += bytes_read as u64;
+            bytes_read
+        })
+    }
+}
+
+impl<R: Read + Seek> Seek for BufReaderWithPos<R> {
+    fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
+        self.reader.seek(pos).map(|pos_num| {
+            self.pos = pos_num;
+            pos_num
+        })
+    }
+}
 #[derive(Debug)]
-pub(super) struct BufWriterWithPos<W>
-where
-    W: Write,
-{
+pub(super) struct BufWriterWithPos<W: Write> {
     pos: u64,
     writer: BufWriter<W>,
+}
+
+impl<W: Write + Seek> BufWriterWithPos<W> {
+    pub(super) fn new(mut w: W) -> io::Result<Self> {
+        let pos = w.seek(SeekFrom::End(0))?;
+        let writer = BufWriter::new(w);
+        Ok(Self { pos, writer })
+    }
+}
+
+impl<W: Write> BufWriterWithPos<W> {
+    pub(super) fn pos(&self) -> u64 {
+        self.pos
+    }
+
+    pub(super) fn get_ref(&self) -> &W {
+        self.writer.get_ref()
+    }
+}
+
+impl<W: Write> Write for BufWriterWithPos<W> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.writer.write(buf).map(|bytes_written| {
+            self.pos += bytes_written as u64;
+            bytes_written
+        })
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.writer.flush()
+    }
+}
+
+impl<W: Write + Seek> Seek for BufWriterWithPos<W> {
+    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
+        self.writer.seek(pos).map(|pos_num| {
+            self.pos = pos_num;
+            pos_num
+        })
+    }
 }
--- src/log.rs
+++ src/log.rs
@@ -1,6 +1,6 @@
 use std::{
     fs,
-    io::{self, Write},
+    io::{self, Seek, Write},
     num::NonZeroUsize,
     path::Path,
     u64, usize,
@@ -8,7 +8,7 @@ use std::{
 
 use bytes::Buf;
 use lru::LruCache;
-use serde::de::DeserializeOwned;
+use serde::{de::DeserializeOwned, Serialize};
 
 use crate::{bufio::BufWriterWithPos, utils, Error};
 
@@ -84,11 +84,35 @@ impl LogDir {
         P: AsRef<Path>,
     {
         match self.0.get_mut(&fileid) {
-            Some(reader) => reader.at(len, pos),
+            Some(reader) => unsafe { reader.at(len, pos) },
             None => {
                 let file = open(utils::datafile_name(path, fileid))?;
                 let mut reader = LogReader::new(file)?;
                 let result = unsafe { reader.at::<T>(len, pos) };
+                self.0.put(fileid, reader);
+                result
+            }
+        }
+    }
+
+    pub(super) unsafe fn copy<P, W>(
+        &mut self,
+        path: P,
+        fileid: u64,
+        len: u64,
+        pos: u64,
+        writer: &mut W,
+    ) -> io::Result<u64>
+    where
+        P: AsRef<Path>,
+        W: Write,
+    {
+        match self.0.get_mut(&fileid) {
+            Some(reader) => unsafe { reader.copy_raw(len, pos, writer) },
+            None => {
+                let file = open(utils::datafile_name(&path, fileid))?;
+                let mut reader = LogReader::new(file)?;
+                let result = unsafe { reader.copy_raw(len, pos, writer) };
                 self.0.put(fileid, reader);
                 result
             }
@@ -112,7 +136,7 @@ impl LogReader {
         T: DeserializeOwned,
     {
         if pos >= self.mmap.len() as u64 {
-            self.mmap = memmap2::MmapOptions::new().map(&self.file)?;
+            self.mmap = unsafe { memmap2::MmapOptions::new().map(&self.file)? };
         }
         let start = pos as usize;
         let end = start + len as usize;
@@ -124,7 +148,7 @@ impl LogReader {
         W: Write,
     {
         if pos >= self.mmap.len() as u64 {
-            self.mmap = memmap2::MmapOptions::new().map(&self.file)?;
+            self.mmap = unsafe { memmap2::MmapOptions::new().map(&self.file)? };
         }
         let start = pos as usize;
         let end = start + len as usize;
@@ -133,6 +157,25 @@ impl LogReader {
 }
 
 pub(super) struct LogWriter(BufWriterWithPos<fs::File>);
+
+impl LogWriter {
+    pub(super) fn new(file: fs::File) -> io::Result<Self> {
+        let writer = BufWriterWithPos::new(file)?;
+        Ok(Self(writer))
+    }
+
+    pub(super) fn append<T: Serialize>(&mut self, entry: &T) -> Result<LogIndex, Error> {
+        let pos = self.0.pos();
+        bincode::serialize_into(&mut self.0, entry)?;
+        self.0.flush()?;
+        let len = self.0.pos() - pos;
+        Ok(LogIndex { len, pos })
+    }
+
+    pub(super) fn sync(&mut self) -> io::Result<()> {
+        self.0.get_ref().sync_all()
+    }
+}
 
 pub(super) fn create<P: AsRef<Path>>(path: P) -> io::Result<fs::File> {
     fs::OpenOptions::new()
